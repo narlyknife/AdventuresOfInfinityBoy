71a22fc9c43885a8b149b426a000aeffa398bede
folke13 <thegamer632@gmail.com>
2017-12-07 15:10:17 +0100

    Anotheegrgre

--- Adventures of Infinity Boy/src/engine/Engine.java
+++ Adventures of Infinity Boy/src/engine/Engine.java
@@ -5,10 +5,13 @@ public class Engine {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//Cancer är gott tycker folke
+		
 		System.out.println("Cancer är gott!!!");
 		for (int i = 1; i < 3; i++) {
 			System.out.println("ja det är de brorsan!!!!");
 		}
+		System.out.println("Cancer är gott!!!");
+		//NICE MAN
 	}
 
 }
