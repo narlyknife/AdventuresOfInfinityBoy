faf9fa16c5683aafb6dd3dacb793c5a9b129994c
folke13 <thegamer632@gmail.com>
2017-12-07 14:11:05 +0100

    Banan sopa

--- Adventures of Infinity Boy/src/engine/Engine.java
+++ Adventures of Infinity Boy/src/engine/Engine.java
@@ -4,8 +4,8 @@ public class Engine {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Cancer är gott");
-		System.out.println("ja det är de brorsan");
+		System.out.println("Cancer är gott!!!");
+		System.out.println("ja det är de brorsan!!!!");
 	}
 
 }
