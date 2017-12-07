cce12e42880e3b9e00111bbef19d83c7d2f1d7d3
narlyknife <erikssonh99@gmail.com>
2017-12-07 14:47:55 +0100

    Skrattar duuuu, förlooorar du mannneeeenn

--- Adventures of Infinity Boy/src/engine/Engine.java
+++ Adventures of Infinity Boy/src/engine/Engine.java
@@ -5,10 +5,10 @@ public class Engine {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//Cancer är gott tycker folke
-    System.out.println("Cancer är gott!!!");
-		System.out.println("ja det är de brorsan!!!!");
-		System.out.println("ja det är de brorsan!!!!");
-		System.out.println("ja det är de brorsan!!!!");
+		System.out.println("Cancer är gott!!!");
+		for (int i = 1; i < 3; i++) {
+			System.out.println("ja det är de brorsan!!!!");
+		}
 	}
 
 }
