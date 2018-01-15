f6f6b0772bf4f9a1517cc48b0f2e3f1be066999a
narlyknife <erikssonh99@gmail.com>
2018-01-15 10:06:21 +0100

    Added FPS and maximum amount of platform variables in init file

--- Adventures of Infinity Boy/src/Init.java
+++ Adventures of Infinity Boy/src/Init.java
@@ -1,4 +1,8 @@
-
 public class Init {
-
+	
+	//Variable holding different Frames Per Second
+	public int[] _FPS = {60,30,20,10};
+	
+	// Variable for maximum amount of platforms that will appear on screen
+	public int platformCount = 10; 	
 }
