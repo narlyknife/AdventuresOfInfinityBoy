7be2d746fc420e7d6c6edaba87aba396cf45d801
folke13 <thegamer632@gmail.com>
2018-01-15 10:58:16 +0100

    Mer Barn.

--- Adventures of Infinity Boy/src/Main.java
+++ Adventures of Infinity Boy/src/Main.java
@@ -1,9 +1,21 @@
+import javax.swing.JFrame;
 
 public class Main {
-
+	public static final JFrame _frame = new JFrame();
+	public static final Init _init = new Init();
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		setFullscreen();
+		
+		
+		
+		_frame.setVisible(true);
 	}
 
+	// Setting "_frame" to fullscreen.
+	private static void setFullscreen() {
+		_frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		_frame.setUndecorated(true);
+	}
+	
 }
