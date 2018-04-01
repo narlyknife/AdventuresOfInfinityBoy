1303b892270810b18e4c3c6f610cad560827b513
folke13 <thegamer632@gmail.com>
2018-04-01 13:58:11 +0200

    Added the ack button to credits and cleaned some code to be more effective in location and size determination.

--- Adventures of Infinity Boy/src/jpanels/Credits.java
+++ Adventures of Infinity Boy/src/jpanels/Credits.java
@@ -14,16 +14,14 @@ import engine.Engine;
 import main.Init;
 
 public class Credits extends JPanel {
-	// CHANGES TO BE MADE
-	//
-	//	- N/A
-	//
 	
 	// Getting values from "init" file
 	static int resX = Init.getResX();
 	static int resY = Init.getResY();
+	
 	static Font font = Init.getFont2();
 	static Font fontItalic = Init.getFont3();
+	static Color textColor = Init.getTextColor();
 	
 	static float scaleX = Init.getScaleIndexX();
 	static float scaleY = Init.getScaleIndexY();
@@ -35,8 +33,8 @@ public class Credits extends JPanel {
 	
 	// Music producers
 	JLabel labelProd = new JLabel();
-	JLabel[] musicProducersArtist = {	new JLabel("Arzee", SwingConstants.CENTER), 				new JLabel("Doze", SwingConstants.CENTER), 				new JLabel("Clever Girl", SwingConstants.CENTER)};
-	JLabel[] musicProducersReal = {		new JLabel("\"Alex Gjersin\"", SwingConstants.CENTER), 		new JLabel("\"Niels Beese\"", SwingConstants.CENTER), 	new JLabel("\"Ryan Kennelley\"", SwingConstants.CENTER)};
+	JLabel[] musicProducersArtist = {	new JLabel("Arzee", SwingConstants.CENTER), 				new JLabel("Doze", SwingConstants.CENTER), 				new JLabel("Clever Girl", SwingConstants.CENTER),			new JLabel("James Stikå", SwingConstants.CENTER)};
+	JLabel[] musicProducersReal = {		new JLabel("\"Alex Gjersin\"", SwingConstants.CENTER), 		new JLabel("\"Niels Beese\"", SwingConstants.CENTER), 	new JLabel("\"Ryan Kennelley\"", SwingConstants.CENTER),	new JLabel("\"James Stikå\"", SwingConstants.CENTER)};
 	
 	// Graphical Artists
 	JLabel labelGrap = new JLabel();
@@ -51,6 +49,7 @@ public class Credits extends JPanel {
 	JLabel button = new JLabel();
 	
 	private static ImageIcon[] imgTitle = Engine.getScaledImageicon("titleCredits", Init.TITLE_SIZE[0] * scaleX, Init.TITLE_SIZE[1] * scaleY);
+	private static ImageIcon[] imgButton = Engine.getScaledImageicon("buttonBack", Init.BUTTON_SIZE_1[0] * scaleX, Init.BUTTON_SIZE_1[1] * scaleY);
 	
 	private static ImageIcon[] imgProg = Engine.getScaledImageicon("titleCreditsProg", Init.TITLE_SIZE_SMALL[0] * scaleX, Init.TITLE_SIZE_SMALL[1] * scaleY);
 	private static ImageIcon[] imgProd = Engine.getScaledImageicon("titleCreditsProd", Init.TITLE_SIZE_SMALL[0] * scaleX, Init.TITLE_SIZE_SMALL[1] * scaleY);
@@ -65,82 +64,102 @@ public class Credits extends JPanel {
 		
 		title.setSize((int) (Init.TITLE_SIZE[0] * scaleX), (int) (Init.TITLE_SIZE[1] * scaleY));
 		title.setLocation((resX - title.getWidth()) / 2, 0);
-		title.setIcon(new ImageIcon(Engine.getScaledImage(Engine.getImage("titleCredits.png"), Init.TITLE_SIZE[0], Init.TITLE_SIZE[1])));
+		title.setIcon(imgTitle[0]);
+		
+		button.setSize((int) (Init.BUTTON_SIZE_1[0] * scaleX), (int) (Init.BUTTON_SIZE_1[1] * scaleY));
+		button.setLocation((resX - button.getWidth()) / 2, (int) (990 * scaleY));
+		button.setIcon(imgButton[0]);
+		
+		Engine.animateLabel(button, imgButton, "mainmenu");
 		
 		int xOffset = (int) (500 * scaleX);
 		int ySpacing = (int) (200 * scaleY);
 		int yOffset = (int) (100 * scaleY);
-		int yOffsetText = (int) (12 * scaleY);
 		
-		labelProg.setSize((int) (Init.TITLE_SIZE_SMALL[0] * scaleX), (int) (Init.TITLE_SIZE_SMALL[1] * scaleY));
+		int titleSizeX = (int) (Init.TITLE_SIZE_SMALL[0] * scaleX);
+		int titleSizeY = (int) (Init.TITLE_SIZE_SMALL[1] * scaleY);
+		
+		int textSizeX = (int) (Init.CREDITS_TEXT_SIZE[0] * scaleX);
+		int textSizeY = (int) (Init.CREDITS_TEXT_SIZE[1] * scaleY);
+		
+		// Programmers Section
+		labelProg.setSize(titleSizeX, titleSizeY);
 		labelProg.setLocation((resX - labelProg.getWidth()) / 2 - xOffset, 2 * ySpacing + yOffset);
-		labelProg.setIcon(new ImageIcon(Engine.getScaledImage(Engine.getImage("titleCreditsProg.png"), Init.TITLE_SIZE_SMALL[0], Init.TITLE_SIZE_SMALL[1])));
+		labelProg.setIcon(imgProg[0]);
 		for(int i = 0; i < programmers.length; i++) {
-			if(i == 0) programmers[i].setLocation(labelProg.getX(), labelProg.getY() + labelProg.getHeight() + yOffsetText);
-			else programmers[i].setLocation(labelProg.getX(), programmers[i-1].getY() + programmers[i-1].getHeight() + yOffsetText);
-			programmers[i].setSize(Init.CREDITS_TEXT_SIZE[0], Init.CREDITS_TEXT_SIZE[1]);
+			if(i == 0) programmers[i].setLocation(labelProg.getX(), labelProg.getY() + labelProg.getHeight());
+			else programmers[i].setLocation(labelProg.getX(), programmers[i-1].getY() + programmers[i-1].getHeight());
+			
+			programmers[i].setSize(textSizeX, textSizeY);
 			programmers[i].setFont(font);
-			programmers[i].setForeground(new Color(255, 255, 255));
+			programmers[i].setForeground(textColor);
 			
 			this.add(programmers[i]);
 		}
 		
-		labelProd.setSize((int) (Init.TITLE_SIZE_SMALL[0] * scaleX), (int) (Init.TITLE_SIZE_SMALL[1] * scaleY));
+		// Music Producers Section
+		labelProd.setSize(titleSizeX, titleSizeY);
 		labelProd.setLocation((resX - labelProd.getWidth()) / 2, ySpacing + yOffset);
-		labelProd.setIcon(new ImageIcon(Engine.getScaledImage(Engine.getImage("titleCreditsProd.png"), Init.TITLE_SIZE_SMALL[0], Init.TITLE_SIZE_SMALL[1])));
+		labelProd.setIcon(imgProd[0]);
 		for(int i = 0; i < musicProducersArtist.length; i++) {
-			if(i == 0) musicProducersArtist[i].setLocation(labelProd.getX(), labelProd.getY() + labelProd.getHeight() + i * yOffsetText);
-			else musicProducersArtist[i].setLocation(musicProducersReal[i-1].getX(), musicProducersReal[i-1].getY() + musicProducersReal[i-1].getHeight() + yOffsetText);
-			musicProducersArtist[i].setSize(Init.CREDITS_TEXT_SIZE[0], Init.CREDITS_TEXT_SIZE[1]);
-			musicProducersArtist[i].setFont(font);
-			musicProducersArtist[i].setForeground(new Color(255, 255, 255));
+			if(i == 0) musicProducersArtist[i].setLocation(labelProd.getX(), labelProd.getY() + labelProd.getHeight());
+			else musicProducersArtist[i].setLocation(musicProducersReal[i-1].getX(), musicProducersReal[i-1].getY() + musicProducersReal[i-1].getHeight());
 			
-			musicProducersReal[i].setLocation(labelProd.getX(), musicProducersArtist[i].getY() + musicProducersArtist[i].getHeight() + yOffsetText);
-			musicProducersReal[i].setSize(Init.CREDITS_TEXT_SIZE[0], Init.CREDITS_TEXT_SIZE[1]);
+			musicProducersArtist[i].setSize(textSizeX, textSizeY);
+			musicProducersArtist[i].setFont(font);
+			musicProducersArtist[i].setForeground(textColor);
+			
+			musicProducersReal[i].setLocation(labelProd.getX(), musicProducersArtist[i].getY() + musicProducersArtist[i].getHeight());
+			musicProducersReal[i].setSize(textSizeX, textSizeY);
 			musicProducersReal[i].setFont(fontItalic);
-			musicProducersReal[i].setForeground(new Color(255, 255, 255));
+			musicProducersReal[i].setForeground(textColor);
 			
 			this.add(musicProducersReal[i]);
 			this.add(musicProducersArtist[i]);
 		}
 		
-		labelGrap.setSize((int) (Init.TITLE_SIZE_SMALL[0] * scaleX), (int) (Init.TITLE_SIZE_SMALL[1] * scaleY));
+		// Graphic Artist Section
+		labelGrap.setSize(titleSizeX, titleSizeY);
 		labelGrap.setLocation((resX - labelGrap.getWidth()) / 2 + xOffset, 2 * ySpacing + yOffset);
-		labelGrap.setIcon(new ImageIcon(Engine.getScaledImage(Engine.getImage("titleCreditsGrap.png"), Init.TITLE_SIZE_SMALL[0], Init.TITLE_SIZE_SMALL[1])));
+		labelGrap.setIcon(imgGrap[0]);
 		for(int i = 0; i < graphicalArtist.length; i++) {
-			if(i == 0) graphicalArtist[i].setLocation(labelGrap.getX(), labelGrap.getY() + labelGrap.getHeight() + yOffsetText);
-			else graphicalArtist[i].setLocation(labelGrap.getX(), graphicalArtist[i-1].getY() + graphicalArtist[i-1].getHeight() + yOffsetText);
-			graphicalArtist[i].setSize(Init.CREDITS_TEXT_SIZE[0], Init.CREDITS_TEXT_SIZE[1]);
+			if(i == 0) graphicalArtist[i].setLocation(labelGrap.getX(), labelGrap.getY() + labelGrap.getHeight());
+			else graphicalArtist[i].setLocation(labelGrap.getX(), graphicalArtist[i-1].getY() + graphicalArtist[i-1].getHeight());
+			
+			graphicalArtist[i].setSize(textSizeX, textSizeY);
 			graphicalArtist[i].setFont(font);
-			graphicalArtist[i].setForeground(new Color(255, 255, 255));
+			graphicalArtist[i].setForeground(textColor);
 			
 			this.add(graphicalArtist[i]);
 		}
 		
-		labelSpec.setSize((int) (Init.TITLE_SIZE_SMALL[0] * scaleX), (int) (Init.TITLE_SIZE_SMALL[1] * scaleY));
+		// Special Thanks Section
+		labelSpec.setSize(titleSizeX, titleSizeY);
 		labelSpec.setLocation((resX - labelSpec.getWidth()) / 2, 3 * ySpacing + yOffset);
-		labelSpec.setIcon(new ImageIcon(Engine.getScaledImage(Engine.getImage("titleCreditsSpec.png"), Init.TITLE_SIZE_SMALL[0], Init.TITLE_SIZE_SMALL[1])));
+		labelSpec.setIcon(imgSpec[0]);
 		for(int i = 0; i < specialThanksCompany.length; i++) {
-			if(i == 0) specialThanksCompany[i].setLocation(labelSpec.getX(), labelSpec.getY() + labelSpec.getHeight() + i * yOffsetText);
-			else specialThanksCompany[i].setLocation(specialThanksName[i-1].getX(), specialThanksName[i-1].getY() + specialThanksName[i-1].getHeight() + yOffsetText);
-			specialThanksCompany[i].setSize(Init.CREDITS_TEXT_SIZE[0], Init.CREDITS_TEXT_SIZE[1]);
-			specialThanksCompany[i].setFont(font);
-			specialThanksCompany[i].setForeground(new Color(255, 255, 255));
+			if(i == 0) specialThanksCompany[i].setLocation(labelSpec.getX(), labelSpec.getY() + labelSpec.getHeight());
+			else specialThanksCompany[i].setLocation(specialThanksName[i-1].getX(), specialThanksName[i-1].getY() + specialThanksName[i-1].getHeight());
 			
-			specialThanksName[i].setLocation(labelSpec.getX(), specialThanksCompany[i].getY() + specialThanksCompany[i].getHeight() + yOffsetText);
-			specialThanksName[i].setSize(Init.CREDITS_TEXT_SIZE[0], Init.CREDITS_TEXT_SIZE[1]);
+			specialThanksCompany[i].setSize(textSizeX, textSizeY);
+			specialThanksCompany[i].setFont(font);
+			specialThanksCompany[i].setForeground(textColor);
+			
+			specialThanksName[i].setLocation(labelSpec.getX(), specialThanksCompany[i].getY() + specialThanksCompany[i].getHeight());
+			specialThanksName[i].setSize(textSizeX, textSizeY);
 			specialThanksName[i].setFont(fontItalic);
-			specialThanksName[i].setForeground(new Color(255, 255, 255));
+			specialThanksName[i].setForeground(textColor);
 			
 			this.add(specialThanksName[i]);
 			this.add(specialThanksCompany[i]);
 		}
 		
+		this.add(title);
 		this.add(labelProg);
 		this.add(labelProd);
 		this.add(labelGrap);
 		this.add(labelSpec);
-		this.add(title);
+		this.add(button);
 	}
 	
 	protected void paintComponent(Graphics g) {
